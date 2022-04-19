75de1484fa87787ea540d6b6da09aa8421a1ca28
brianreicher <reicher.b@northeastern.edu>
2022-04-19 19:59:05 -0400

    Regression implementation

--- Exponential_Predicting/nn_model_train.py
+++ Exponential_Predicting/nn_model_train.py
@@ -5,9 +5,11 @@ import pandas as pd
 import tensorflow
 from tensorflow import keras
 from keras.utils.vis_utils import plot_model
+from keras import backend as K
 from sklearn.model_selection import train_test_split
 from generate_data import data
 
+# TODO: REGRESSION INSTEAD OF CLASSIFICATION
 
 # - Consider the exponential function f(x)=exp(-x/c), with x=0,1,2,….,L-1 integers
 # - Generate many training datasets {f(0,f(1),f(2),…f(L-1)} for different values of c
@@ -16,8 +18,8 @@ from generate_data import data
 # Initate Dataloader - pick l-value of choice
 dataloader = data(7)
 drop_vals = np.arange(len(dataloader.columns)-1).tolist()
-x_train, x_valid, y_train, y_valid = train_test_split(dataloader.drop(columns='c'), 
-                                                        dataloader.drop(columns = drop_vals), 
+x_train, x_valid, y_train, y_valid = train_test_split(dataloader.drop(columns='c').to_numpy(), 
+                                                        dataloader.drop(columns = drop_vals).to_numpy(), 
                                                         test_size=0.2, random_state=0)
 
 # Initate model
@@ -26,10 +28,13 @@ def load_model():
     model.add(keras.layers.Dense(1000, activation="relu"))
     model.add(keras.layers.Dense(300, activation="relu"))
     model.add(keras.layers.Dense(100, activation="relu"))
-    model.add(keras.layers.Dense(len(dataloader.columns)-1, activation="softmax"))
-    print(model.summary())
-    keras.utils.plot_model(model, "model_graph.png", show_shapes=True)
+    model.add(keras.layers.Dense(1))
+    # print(model.summary())
+    # keras.utils.plot_model(model, "model_graph.png", show_shapes=True)
     return model
+
+def rmse(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
 
 model = load_model()
 
@@ -38,10 +43,10 @@ np.random.seed(42)
 tensorflow.random.set_seed(42)
 model.compile(loss="sparse_categorical_crossentropy",
             optimizer="sgd",
-            metrics=["accuracy"])
+            metrics=[rmse])
 history = model.fit(x_train, y_train,
         batch_size=128, epochs=30,
-        verbose=1,
-        validation_data=(x_valid, y_valid))
+        verbose=1)
 
-model.evaluate(x_valid, y_valid)
+
+print(model.evaluate(x_valid, y_valid))
