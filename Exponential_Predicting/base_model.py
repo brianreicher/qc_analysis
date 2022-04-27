04eab4288f5b9399d577f07adc4bdb26a92b55cf
brianreicher <reicher.b@northeastern.edu>
2022-04-27 18:30:22 -0400

    Dataloader updates

--- Exponential_Predicting/base_model.py
+++ Exponential_Predicting/base_model.py
@@ -1,39 +1,40 @@
+import random
 import numpy as np
 import tensorflow
-import keras
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dense, Dropout
 from tensorflow.python.keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from generate_data import data
-
+from sklearn.model_selection import train_test_split
 
 # Regression testing
-l_val = 7
-dataloader = data(l_val)
+c_range = random.randint(50, 60)
+dataloader = data(c_range)
 drop_vals = np.arange(len(dataloader.columns)-1).tolist()
-x_train, x_valid, y_train, y_valid = train_test_split(dataloader.drop(columns = drop_vals).to_numpy(), 
+x_train, x_valid, y_train, y_valid = train_test_split(dataloader.drop(columns=drop_vals).to_numpy(),
                                                       dataloader.drop(columns='c').to_numpy(), test_size=0.2, 
                                                       random_state=0)
 
-def load_model(): #Function to load model upon calling
-    model = Sequential()
-    model.add(Dense(1, activation="relu")) #Attempts to avoid overfitting data
-    model.add(Dense(10, activation="relu"))
-    model.add(Dense(15, activation="relu"))
-    model.add(Dense(25, activation="relu"))
-    model.add(Dense(12, activation="relu"))
-    model.add(Dense(7))
-    return model
+
+def load_model():  # Function to load model upon calling
+    seq = Sequential()
+    seq.add(Dense(75, activation="relu"))  # Attempts to avoid over-fitting data
+    seq.add(Dense(250, activation="relu"))
+    seq.add(Dropout(0.2))
+    seq.add(Dense(100, activation="relu"))
+    seq.add(Dense(90, activation="relu"))
+    seq.add(Dense(c_range))
+    return seq
 
 
-model = load_model() #Initiate model
-keras.backend.clear_session() #Clear model backend and set random state
+model = load_model()  # Initiate model
 np.random.seed(42)
 tensorflow.random.set_seed(42)
 
-model.compile(optimizer='Adam', #Complile model
-              loss=['mean_squared_error', 'huber_loss'],
-              metrics=['cosine_similarity', 'hinge'])
+model.compile(optimizer='Adam',  # Compile model
+              loss=['mse'],
+              metrics=['accuracy'])  # Figure out hinge
 
-history = model.fit(x_train, [y_train], epochs=60, batch_size=20,
-                    validation_data=(x_valid, y_valid)) #Fit model
+history = model.fit(x_train, y_train, epochs=60, batch_size=20,
+                    validation_data=(x_valid, y_valid))  # Fit model
+
--- Exponential_Predicting/generate_data.py
+++ Exponential_Predicting/generate_data.py
@@ -1,31 +1,34 @@
+import random
 import numpy as np
 import pandas as pd
 
-#Returns a dataframe of calculated exponential functions for given C-parameters and L-angular quantum numbers
+
+# Returns a dataframe of calculated exponential functions for given C-parameters and L-angular quantum numbers
 class GenerateData:
-    def __init__(self, l):
-        self.l = l
-        self.x = np.arange(l)
-        self.c = np.arange(start=0.0001, stop=1, step = 0.0001)
+    def __init__(self, interval):
+        self.interval = interval
+        self.x = np.arange(interval)
+        self.c = np.arange(start=0.0001, stop=1, step=0.0001)
 
-    #Evaluate function for given c-values
+    # Evaluate function for given c-values
     @staticmethod
-    def func(input, arg):
-        return np.e**(-input/arg)
+    def func(x, arg):
+        return np.e**(-x/arg)
 
-    #Create dataset: 100,000 funcs?
+    # Create dataset: 100,000 funcs?
     def populate(self):
         func_df = pd.DataFrame(index=self.c.tolist())
         for i in self.x:
             col = []
             for j in self.c:
-                col.append(GenerateData(self.l).func(input = i, arg = j))
+                col.append(GenerateData(self.interval).func(x=i, arg=j))
             func_df[i] = col
         func_df['c'] = self.c
         return func_df
 
 
-#Set param value for integer L-value (angular momentum quantum number)
-def data(l: int):
-    return GenerateData(l).populate()
+# Set param value for integer L-value (angular momentum quantum number)
+def data(integer: int):
+    return GenerateData(integer).populate()
+
 
